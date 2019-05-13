061980ee73558324aeb607f4aa9fa8c056885a16
heytitle <pat.chormai@gmail.com>
2019-05-13 23:53:28 +0700

    update file path

--- Formula/thai-dict.rb
+++ Formula/thai-dict.rb
@@ -1,5 +1,6 @@
 class ThaiDict < Formula
-    url "https://drive.google.com/uc?export=download&confirm=zZFR&id=0BzgSk7QM-LIFWDRLai0tTlgwT00"
+    version "1.0.1"
+    url "https://www.dropbox.com/s/dl/yf96jb79xpnbfor/mac-thai-dict-#{version}.pkg"
     sha256 "63c5461c6c36a85e2543f78ce6f2e70038ecb76edb0862ef1cfd69a059dcd06d"
     desc "Thai-Eng Dictionary"
 
