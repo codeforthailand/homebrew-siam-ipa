136735f46073593cd0dc8de69ae717983cfff6b6
heytitle <pat.chormai@gmail.com>
2019-05-14 01:23:35 +0700

    update source for dict

--- Casks/thai-dict.rb
+++ Casks/thai-dict.rb
@@ -1,7 +1,7 @@
 cask 'thai-dict' do
     version '1.0.1'
   
-    url "https://www.dropbox.com/s/dl/bfwer9f3fjmnuwq/mac-thai-dict-#{version}.pkg"
+    url "https://storage.googleapis.com/codeforthailand/mac-thai-dict-#{version}.pkg"
     sha256 "c4832b5d57106b47d3af36b2c562ced348fd25462727a92730291aa828f0a926"
 
     pkg "mac-thai-dict-#{version}.pkg"
