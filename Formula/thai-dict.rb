abc3d7b0315ab2d8fed9747d53e86c66dcdd15ec
heytitle <pat.chormai@gmail.com>
2019-05-14 00:01:16 +0700

    update formula

--- Formula/thai-dict.rb
+++ Formula/thai-dict.rb
@@ -1,10 +1,12 @@
 class ThaiDict < Formula
     version "1.0.1"
-    url "https://www.dropbox.com/s/dl/yf96jb79xpnbfor/mac-thai-dict-#{version}.pkg"
+    url "https://www.dropbox.com/s/dl/p77opxpuiajq8w6/mac-thai-dict-#{version}.tar.gz"
     sha256 "c4832b5d57106b47d3af36b2c562ced348fd25462727a92730291aa828f0a926"
     desc "Thai-Eng Dictionary"
 
     def install
         puts "Test install siam-ipa"
+        system "installer", 
+            ""
     end
 end
