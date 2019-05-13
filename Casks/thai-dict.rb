57b843aaedadd028c18f6a80c9552d1a834a3d6c
heytitle <pat.chormai@gmail.com>
2019-05-14 00:47:26 +0700

    update cask

---
+++ Casks/thai-dict.rb
@@ -0,0 +1,10 @@
+cask 'mac-thai-dict' do
+    version '1.0.1'
+  
+    url "https://www.dropbox.com/s/dl/bfwer9f3fjmnuwq/mac-thai-dict-#{version}.pkg"
+    sha256 "c4832b5d57106b47d3af36b2c562ced348fd25462727a92730291aa828f0a926"
+
+    pkg "mac-thai-dict-#{version}.pkg"
+  
+    uninstall pkgutil: 'com.infinisoft.thaiDictionaries.thaiDictionaries.pkg'
+ end
