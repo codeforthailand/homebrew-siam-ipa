141dc84df4522fb1b7c1e8540bc93eb543f06f5b
heytitle <pat.chormai@gmail.com>
2019-05-13 23:16:47 +0700

    add sha256 for thai-dict

--- Formula/thai-dict.rb
+++ Formula/thai-dict.rb
@@ -1,7 +1,8 @@
 class ThaiDict < Formula
     url "https://drive.google.com/uc?export=download&confirm=zZFR&id=0BzgSk7QM-LIFWDRLai0tTlgwT00"
-
+    sha256 "63c5461c6c36a85e2543f78ce6f2e70038ecb76edb0862ef1cfd69a059dcd06d"
     desc "Thai-Eng Dictionary"
+
     def install
         puts "Test install siam-ipa"
     end
