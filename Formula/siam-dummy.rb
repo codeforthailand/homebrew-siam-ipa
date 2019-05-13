2e937af1153de5c87b66a342fcc53ae3a1490302
heytitle <pat.chormai@gmail.com>
2019-05-13 23:04:26 +0700

    update dummy formula

--- Formula/siam-dummy.rb
+++ Formula/siam-dummy.rb
@@ -1,4 +1,5 @@
 class SiamDummy < Formula
+    url "https://github.com/codeforthailand/homebrew-siam"
     desc "test formula for siam-ipa tap"
     def install
         puts "Test install siam-ipa"
