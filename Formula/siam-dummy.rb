0d78b6f2c114082575f36d9a6517695c966f4c0a
heytitle <pat.chormai@gmail.com>
2019-05-13 23:14:44 +0700

    introduce dict formula

--- Formula/siam-dummy.rb
+++
@@ -1,7 +0,0 @@
-class SiamDummy < Formula
-    url "https://github.com/codeforthailand/homebrew-siam"
-    desc "test formula for siam-ipa tap"
-    def install
-        puts "Test install siam-ipa"
-    end
-end
---
+++ Formula/thai-dict.rb
@@ -0,0 +1,8 @@
+class ThaiDict < Formula
+    url "https://drive.google.com/uc?export=download&confirm=zZFR&id=0BzgSk7QM-LIFWDRLai0tTlgwT00"
+
+    desc "Thai-Eng Dictionary"
+    def install
+        puts "Test install siam-ipa"
+    end
+end
