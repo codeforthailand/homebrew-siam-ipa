5a00b87f02e2248bbeeeef07d27d29681da665e5
heytitle <pat.chormai@gmail.com>
2019-05-13 23:55:21 +0700

    update sha256

--- Formula/thai-dict.rb
+++ Formula/thai-dict.rb
@@ -1,7 +1,7 @@
 class ThaiDict < Formula
     version "1.0.1"
     url "https://www.dropbox.com/s/dl/yf96jb79xpnbfor/mac-thai-dict-#{version}.pkg"
-    sha256 "63c5461c6c36a85e2543f78ce6f2e70038ecb76edb0862ef1cfd69a059dcd06d"
+    sha256 "c4832b5d57106b47d3af36b2c562ced348fd25462727a92730291aa828f0a926"
     desc "Thai-Eng Dictionary"
 
     def install
